835dc001424af2f0166c731e9597f959a972f4fd
Antoine Lavallée <antoine.lavallee@flyable.dev>
2024-09-11 09:45:45 -0400

    structure to allow project integration

--- example/example_general.cpp
+++ example/example_general.cpp
@@ -1,7 +1,7 @@
 #include "Cow3D.h"
 #include "render/MeshBuilder.h"
 
-int main() {
+void example0() {
 
     cow::Cow3D engine("Cow3D");
 
@@ -9,11 +9,16 @@ int main() {
     engine.getAudio().playSound(engine.getRes().getSound("chest"));
     engine.getAudio().playSound(engine.getRes().getSound("thune"));
 
-    auto animation = engine.getScene3D().addAnimation(cow::Vec3f(0, 0, 2), engine.getRes().getModel("arthur"), 1);
-    animation->getModel().playAnimation("running", true);
+    for (int x = 0; x < 5; ++x) {
+        for (int y = 0; y < 5; ++y) {
+            auto animation = engine.getScene3D().addAnimation(cow::Vec3f(x - 2, y - 2, 3),
+                                                              engine.getRes().getModel("arthur"),
+                                                              1);
+            animation->getModel().playAnimation("running", true);
+        }
+    }
 
-
-    auto text = engine.getScene2D().addText(cow::Vec2f(0, 0), "Cow3D !", 64);
+    auto text = engine.getScene2D().addText(cow::Vec2f(0, 100), "Cow3D !", 64);
     text->setColor(cow::Color::yellow());
 
     bool run = true;
