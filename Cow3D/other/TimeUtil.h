8c2dbcbfc032420a641221a2fbe2bf501fc9dadd
Antoine Lavallée <antoine.lavallee@flyable.dev>
2024-09-14 23:13:38 -0400

    Port time utile to sdl3

--- Cow3D/other/TimeUtil.h
+++ Cow3D/other/TimeUtil.h
@@ -2,13 +2,13 @@
 #define EVOCLIENT_TIMEUTIL_H
 
 #include <chrono>
-#include <SDL.h>
+#include <SDL3/SDL.h>
 
 class TimeUtil {
 public:
 
     static float getCurrentTimeInSeconds() {
-        return static_cast<float>(SDL_GetTicks64()) / 1000.f;
+        return static_cast<float>(SDL_GetTicks()) / 1000.f;
     }
 
 };
